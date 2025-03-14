d6774f224ad2765c9c869f39aa51c5e65b0e468d
ConstantDust <Carteryackovich@gmail.com>
2025-03-14 00:48:42 -0400

    Almost forgor

--- src/main/java/net/dustley/clean_cut/CleanCut.java
+++ src/main/java/net/dustley/clean_cut/CleanCut.java
@@ -23,7 +23,7 @@ public class CleanCut implements ModInitializer {
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// Proceed with mild coral.
 
 		ModEnchantments.initialize();
 		ModEntities.registerModEntities();
